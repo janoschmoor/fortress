cfca1209bcde072408714b3b68bd93573181140d
Janosch Moor <janosch.moor@gmail.com>
2024-08-14 23:43:33 +0200

    adding small weather display

--- sentry/app/dashboard/page.tsx
+++ sentry/app/dashboard/page.tsx
@@ -1,19 +1,12 @@
 
-"use client"
+"use client";
 
 import MapDisplay from '@/components/MapDisplay';
 import UnitDisplay from '../../components/UnitDisplay';
 import { fetchData } from '../lib/actions';
+import WeatherDisplay from '@/components/WeatherDisplay';
 
 export default function DashboardPage() {
-
-
-    // test fetch data
-    var handler = () => {
-        fetchData().then((data) => {
-            console.log(data);
-        });
-    }
 
 
     return (
@@ -25,9 +18,9 @@ export default function DashboardPage() {
                 <UnitDisplay unit="exampleUnit3" />
                 <UnitDisplay unit="exampleUnit2" />
                 <MapDisplay />
+                <WeatherDisplay />
                 {/* Add more UnitDisplay components as needed */}
             </div>
-            <button onClick={handler} className="btn btn-primary">Refresh</button>
         </div>
     );
 };
--- sentry/app/lib/actions.ts
+++ sentry/app/lib/actions.ts
@@ -2,6 +2,6 @@
 // fetch test function
 export async function fetchData() {
     //var data = fetch("https://api.open-meteo.com/v1/forecast?latitude=47.37&longitude=8.54&hourly=temperature_2m");
-    var data = fetch("https://api.open-meteo.com/v1/forecast?latitude=47.37&longitude=8.54&hourly=cloud_cover&daily=weather_code");
+    var data = fetch("https://api.open-meteo.com/v1/forecast?latitude=47.37&longitude=8.54&daily=weather_code");
     return (await data).json();
 }
